d62008b39c4d5f8d2724a6282728ea55252b9fc0
ibadix <ibadix.com@gmail.com>
2017-01-01 01:09:45 +0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,6 +113,10 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
