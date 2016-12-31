35945cc78a37bdec2a59b15f96bd08735e6044c8
ibadix <ibadix.com@gmail.com>
2017-01-01 01:07:23 +0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,10 +101,17 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
